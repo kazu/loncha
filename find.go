82a5422c272cdfb835e4040c9495e6c1c412685e
xtakei <xtakei@me.com>
2019-03-04 00:36:13 +0900

    DOC of FInd/IndexOf

--- find.go
+++ find.go
@@ -1,5 +1,17 @@
+// Copyright 2019 Kazuhisa TAKEI<xtakei@rytr.jp>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package finder for slice.
+//
+// To Find from slice: (list is slice)
+//   lonacha.Find(&list, func(i int) {
+//	 	return list[i].ID == 555
+//}
+
 package lonacha
 
+// Find is value of slice if fn is true. if slice is not pointer of slice or empty, return error
 func Find(slice interface{}, fn CondFunc) (interface{}, error) {
 
 	idx, err := IndexOf(slice, fn)
@@ -13,6 +25,8 @@ func Find(slice interface{}, fn CondFunc) (interface{}, error) {
 
 }
 
+// IndexOf gets the index at which the firsh match fn is true. if not found. return -1.
+// return error if slice is not pointer of the slice.
 func IndexOf(slice interface{}, fn CondFunc) (int, error) {
 
 	rv, err := slice2Reflect(slice)
