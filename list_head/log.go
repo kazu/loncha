26acaa3e68fecee6d17a18483d8e8c2e0fc26446
xtakei <xtakei@me.com>
2019-04-02 03:28:23 +0900

    ADD logger for list_head

---
+++ list_head/log.go
@@ -0,0 +1,13 @@
+package list_head
+
+import "go.uber.org/zap"
+
+func Log(enableDebug bool) *zap.Logger {
+	if enableDebug {
+		log, _ := zap.NewDevelopment()
+		return log
+	}
+
+	log, _ := zap.NewProduction()
+	return log
+}
