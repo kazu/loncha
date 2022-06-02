4aa42a592a8c4c48df448f37803b108cb98b6a22
xtakei <xtakei@me.com>
2022-06-03 02:52:48 +0900

    Add SelectMap()
    SelectMap() provide to rewrite map each key, value pair.

--- map.go
+++ map.go
@@ -25,3 +25,17 @@ func Values[K comparable, V any](m map[K]V) (values []V) {
 	}
 	return
 }
+
+// SelectMap ... rewrite map each key, value pair.
+func SelectMap[K comparable, V any](m map[K]V, fn func(k K, v V) (K, V, bool)) (result map[K]V) {
+	result = make(map[K]V)
+	for k, v := range m {
+
+		nk, nv, remove := fn(k, v)
+		if remove {
+			continue
+		}
+		result[nk] = nv
+	}
+	return
+}
