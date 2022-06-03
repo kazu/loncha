1c209a79fd96d3639080fb91985c980760719aa0
xtakei <xtakei@me.com>
2022-06-04 04:52:16 +0900

    Inject(): change type of InjectFn

--- inject.go
+++ inject.go
@@ -1,13 +1,13 @@
 package loncha
 
 // InjectFn ... function for Inject()
-type InjectFn[T any, R any] func(*R, T) R
+type InjectFn[T any, R any] func(R, T) R
 
 // Inject ... return an object formed from operands via InjectFn
 func Inject[T any, V any](s []T, injectFn InjectFn[T, V]) (v V) {
 
 	for _, t := range s {
-		v = injectFn(&v, t)
+		return injectFn(v, t)
 	}
 	return
 }
@@ -23,15 +23,16 @@ type SumIdent[T any, V Ordered] func(e T) V
 // Sum ... sum of slice values in Ordered type
 func Sum[T Ordered](s []T) (v T) {
 
-	return Inject(s, func(result *T, e T) T {
-		return *result + e
+	return Inject(s, func(result T, e T) T {
+		return result + e
 	})
 
 }
 
 // SumWithFn ... sum of slice values in non-Ordered type with SumIdent()
 func SumWithFn[T any, V Ordered](s []T, fn SumIdent[T, V]) (v V) {
-	return Inject(s, func(result *V, e T) V {
-		return *result + fn(e)
+	return Inject(s, func(result V, e T) V {
+		v := result + fn(e)
+		return v
 	})
 }
